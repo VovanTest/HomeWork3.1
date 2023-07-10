668ca8748361a112b2b4063482cca69bfcc6a625
VladimirTikhonov <Vovantikhonov@inbox.ru>
2023-07-11 00:56:35 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,6 @@
+public class BmiService {
+    public int calculate (double kgWeight, double mHeight) {
+        double result = kgWeight / (mHeight * mHeight);
+        return (int) result;
+    }
+}
