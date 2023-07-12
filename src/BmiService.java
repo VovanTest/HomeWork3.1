2200a8312806a67e98a1f58873c37297cad1dba5
VladimirTikhonov <Vovantikhonov@inbox.ru>
2023-07-12 23:40:51 +0300

    Rechanged gitignore

--- src/BmiService.java
+++
@@ -1,6 +0,0 @@
-public class BmiService {
-    public int calculate (double kgWeight, double mHeight) {
-        double result = kgWeight / (mHeight * mHeight);
-        return (int) result;
-    }
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BmiService service = new BmiService ();
-        double kgWeight = 98;
-        double mHeight = 1.87;
-        int bmi = service.calculate(kgWeight,mHeight);
-        System.out.println(bmi);
-    }
-}
